f36aeacb3371896e54d4061b449642075874cbd3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-14 12:26:56 +0800

    Update author_sol.cpp

--- author_sol.cpp
+++ author_sol.cpp
@@ -16,7 +16,7 @@ int main() {
 		cin >> a >> p;
 		// store the minimum value between 'cost' and 'p'
 		// to make sure that we get the lowest price, and
-		// once we get it, we will use that price of that day
+		// once we get it, we will use the price of that day
 		// to buy all the meat that we need since it was mentioned
 		// in the problem statement that "he can keep the meat he
 		// has for the future"
